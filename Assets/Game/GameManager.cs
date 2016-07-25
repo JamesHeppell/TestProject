afa8c04dffdfcd8627247105ad00565c613c8880
James Heppell <jheppell100@hotmail.com>
2016-07-25 16:32:25 +0100

    Added stars and level scoring

--- Assets/Game/GameManager.cs
+++ Assets/Game/GameManager.cs
@@ -1,25 +1,40 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class GameManager : MonoBehaviour {
 
+	public Text levelScoreDisplay;
 	public bool recording = true; // for recording replays
 
+	private GameObject starParent;
+	private RectTransform[] starRectTransforms;
+	private int numberStars;
+	private int starsCollected = 0;
 	
 	void Start () {
-		PlayerPrefsManager.UnlockLevel(4); //unlock first level (02a level1)
-
-	}
+		starParent = GameObject.Find("Stars");
+		starRectTransforms = starParent.GetComponentsInChildren<RectTransform>();
+		numberStars = starRectTransforms.Length;
+	} 
 	
+	public void AddCollectedStar(){
+		starsCollected ++;
+	}
 
 	void Update () {
 
+	UpdateLevelScoreDisplay();
 //		//For recording replays
 //		if (CrossPlatformInputManager.GetButton("Fire1")){
 //			recording = false;
 //		}else {
 //			recording = true;
 //		}
+	}
+
+	private void UpdateLevelScoreDisplay(){
+		levelScoreDisplay.text = starsCollected.ToString() + "/" + numberStars.ToString(); 
 	}
 }
---
+++ Assets/Star/Star.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Star : MonoBehaviour {
+
+	private GameManager gameManager;
+
+	void Start(){
+		gameManager = FindObjectOfType<GameManager>();
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if (collider.tag == "Player"){
+			Debug.Log("Start collected");
+			gameManager.AddCollectedStar();
+			Destroy(this.gameObject);
+		}
+
+	}
+}
